7a3a784732c379c314eb42df9bff34f58aeb6de8
rterra90 <82670242+rterra90@users.noreply.github.com>
2021-07-05 11:11:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ animaIntro()
 
 function animaIntro(){
   const animaItens = document.querySelectorAll('[data-anima]')
-  animaItens.forEach((item, index)=>{
+  animaItens.forEach((item)=>{
     let time = item.dataset.time;
     setTimeout(()=>{
       item.classList.add('anima-center')
@@ -11,11 +11,15 @@ function animaIntro(){
 }
 
 
-/* HANDLE SECTIONS*/
+
+/* HANDLE SECTIONS WHEEL*/
 const sections = document.querySelectorAll('main > section')
 const itensMenu = document.querySelectorAll('aside li')
-
+const itensMenuBars = document.querySelectorAll('.nav-btn > div')
+const btnsIntro = document.querySelectorAll('.btns > span');
 document.querySelector('main').addEventListener('wheel', handleSections)
+
+
   
 function handleSections(event){
   let currentSection = document.querySelector('.active').dataset.section;
@@ -25,10 +29,11 @@ function handleSections(event){
         sections[currentSection].classList.add('animate-up-out');
         setTimeout(()=>{
           sections[currentSection].classList.add('d-none');
-          sections[currentSection].classList.remove('animate-up-out');
-          sections[currentSection].classList.remove('active');
+          sections[currentSection].classList.remove('animate-up-out', 'active');
           itensMenu[currentSection].classList.remove('menu-active')
           itensMenu[+currentSection+1].classList.add('menu-active')
+          itensMenuBars[currentSection].classList.remove('active-nav')
+          itensMenuBars[+currentSection+1].classList.add('active-nav')
   
         },700);
   
@@ -49,8 +54,7 @@ function handleSections(event){
         sections[currentSection].classList.add('animate-down-out');
         setTimeout(()=>{
           sections[currentSection].classList.add('d-none');
-          sections[currentSection].classList.remove('animate-down-out');
-          sections[currentSection].classList.remove('active');
+          sections[currentSection].classList.remove('animate-down-out', 'active');
         },700);
   
         setTimeout(() => {
@@ -58,6 +62,8 @@ function handleSections(event){
           sections[+currentSection-1].classList.remove('d-none');
           itensMenu[currentSection].classList.remove('menu-active')
           itensMenu[+currentSection-1].classList.add('menu-active')
+          itensMenuBars[currentSection].classList.remove('active-nav')
+          itensMenuBars[+currentSection-1].classList.add('active-nav')
         }, 900);
   
         setTimeout(() => {
@@ -70,18 +76,24 @@ function handleSections(event){
 
 }
 
-/* MENU LATERAL */
-itensMenu.forEach((item, index)=>{
-  let i = index;
-  item.addEventListener('click', ()=>{
-    let currentSection = document.querySelector('.active').dataset.section;
+/* HANDLE SECTIONS MENU LATERAL */
+itensMenu.forEach((item)=>{
+  item.addEventListener('click', handleSectionsMenu)
+})
 
+itensMenuBars.forEach((item)=>{
+  item.addEventListener('click', handleSectionsMenu)
+})
+
+  
+  function handleSectionsMenu(event){
+    let currentSection = document.querySelector('.active').dataset.section;
+    let i = event.target.dataset.order;
     if(currentSection > i){
       sections[currentSection].classList.add('animate-down-out');
       setTimeout(()=>{
           sections[currentSection].classList.add('d-none');
-          sections[currentSection].classList.remove('animate-down-out');
-          sections[currentSection].classList.remove('active');
+          sections[currentSection].classList.remove('animate-down-out', 'active');
         },700);
 
         setTimeout(() => {
@@ -89,6 +101,8 @@ itensMenu.forEach((item, index)=>{
             sections[i].classList.remove('d-none');
             itensMenu[currentSection].classList.remove('menu-active')
             itensMenu[i].classList.add('menu-active')
+            itensMenuBars[currentSection].classList.remove('active-nav')
+            itensMenuBars[i].classList.add('active-nav')
           }, 900);
           
           setTimeout(() => {
@@ -100,10 +114,11 @@ itensMenu.forEach((item, index)=>{
       
       setTimeout(()=>{
           sections[currentSection].classList.add('d-none');
-          sections[currentSection].classList.remove('animate-up-out');
-          sections[currentSection].classList.remove('active');
+          sections[currentSection].classList.remove('animate-up-out', 'active');
           itensMenu[currentSection].classList.remove('menu-active')
-      itensMenu[i].classList.add('menu-active')
+          itensMenu[i].classList.add('menu-active');
+          itensMenuBars[currentSection].classList.remove('active-nav')
+          itensMenuBars[i].classList.add('active-nav')
         },700);
 
         setTimeout(() => {
@@ -117,7 +132,53 @@ itensMenu.forEach((item, index)=>{
             sections[i].classList.add('active');
           }, 1300);
     }
+  }
 
+  // botões intro
+btnsIntro.forEach((btn, i)=>{
+  btn.addEventListener('click', ()=>{
+    if(i==0){
+      sections[i].classList.add('animate-up-out');
+      setTimeout(()=>{
+        sections[i].classList.add('d-none');
+        sections[i].classList.remove('animate-up-out', 'active');
+        itensMenu[i].classList.remove('menu-active')
+        itensMenu[1].classList.add('menu-active');
+        itensMenuBars[i].classList.remove('active-nav')
+        itensMenuBars[1].classList.add('active-nav')
+      },700);
 
+      setTimeout(() => {
+        sections[1].classList.add('animate-up-in');
+         sections[1].classList.remove('d-none');
+         
+       }, 900);
+       
+       setTimeout(() => {
+         sections[1].classList.remove('animate-up-in');
+         sections[1].classList.add('active');
+       }, 1300);
+    }else{
+      sections[0].classList.add('animate-up-out');
+      setTimeout(()=>{
+        sections[0].classList.add('d-none');
+        sections[0].classList.remove('animate-up-out', 'active');
+        itensMenu[0].classList.remove('menu-active')
+        itensMenu[3].classList.add('menu-active');
+        itensMenuBars[0].classList.remove('active-nav')
+        itensMenuBars[3].classList.add('active-nav')
+      },700);
+
+      setTimeout(() => {
+        sections[3].classList.add('animate-up-in');
+         sections[3].classList.remove('d-none');
+         
+       }, 900);
+       
+       setTimeout(() => {
+         sections[3].classList.remove('animate-up-in');
+         sections[3].classList.add('active');
+       }, 1300);
+    }
   })
 })
